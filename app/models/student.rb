90fd4a9c60e6b3415d36685daafbe14d0760cff5
Jonathan Chambers <j.chambers@gmx.net>
2013-08-01 23:28:01 +0100

    added sorting hat method and specs

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,13 @@
 class Student < ActiveRecord::Base
+  
   attr_accessible :name
   belongs_to :house, inverse_of: :students
+  before_create :put_on_the_sorting_hat!
+  
+  private
+  
+  def put_on_the_sorting_hat!
+    self.house = House.order("RANDOM()").first
+  end
+  
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,29 +2,24 @@ require 'spec_helper'
 
 describe Student do
 
-  context "with house unassigned" do
-    
-    before(:each) do
-      @student = FactoryGirl.create(:student, house: nil)
-    end
-    
-    it "returns nil for the house association" do
-      @student.house.should be_nil
+  before(:each) do
+    @houses  = FactoryGirl.create_list(:house, 4)
+    @student = Student.new(name: "Harry Potter")
+  end
+  
+  describe ".put_on_the_sorting_hat!" do
+  
+    it "associates the student with a randomly selected house" do
+      #TODO figure out best way to test for proper random assignment
+      @student.send(:put_on_the_sorting_hat!)
+      @student.house.should be_an_instance_of(House)
     end
     
   end
   
-  context "with house assigned" do
-    
-    before(:each) do
-      @house   = FactoryGirl.create(:house)
-      @student = FactoryGirl.create(:student, house: @house)
-    end
-    
-    it "is associated with its house" do
-      @student.house.should be(@house)
-    end
-    
+  it "is associated with a house automatically on creation" do
+    @student.save
+    @student.house.should be_an_instance_of(House)
   end
 
 end
